1ccbb5df00bde899b631de5c08134076df075b27
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-10-21 10:49:22 -0300

    feat: add base model at payload

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ import joblib
 import numpy as np
 
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Payload(BaseModel):
+  lyrics: str
 
 @app.get("/")
 def home():
   return {'message': 'See docs: https://classification-unip.herokuapp.com/docs'}
 
 @app.post('/api/lyrics/v1')
-def classification_lyrics(lyrics: str):
+def classification_lyrics(payload: Payload):
   model = joblib.load('model/classificationLyrics.unip')
   
-  band = model.predict([lyrics])
-  probability = model.predict_proba([lyrics])
+  band = model.predict([payload.lyrics])
+  probability = model.predict_proba([payload.lyrics])
   index = np.argmax(probability)
 
-  data = { 'band': band[0], 'probability': probability[0][index], 'lyrics': lyrics }
+  data = { 'band': band[0], 'probability': probability[0][index], 'lyrics': payload.lyrics }
 
   return data
