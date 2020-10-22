90297dc22946a3e8014695e28aa6debc5b2238b8
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-10-22 15:15:22 -0300

    feat: add validation at minimum words

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ def home():
 @app.post('/api/lyrics/v1')
 def classification_lyrics(payload: Payload):
   model = joblib.load('classificationLyrics.unip')
+
+  wordsCounter = len(payload.lyrics.split())
+  minimumWords = 50
+
+  if (wordsCounter < minimumWords):
+    return { 'wordsCounter': wordsCounter, 'minimumWords': minimumWords, 'message': 'Quantidade mínima de palavras não atingida, por favor, considere pegar uma parte da letra maior.' }
   
   band = model.predict([payload.lyrics])
   probability = model.predict_proba([payload.lyrics])
